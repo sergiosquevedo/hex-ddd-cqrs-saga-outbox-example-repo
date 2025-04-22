d0af3ae38f46e39d4a3b0328e720258a0292184a
Sergio S. Quevedo <sergio.sanchez@sngular.com>
2025-04-22 17:11:55 +0200

    Delete zookeeper and use KRaft instead

--- infrastructure/kafka/kafka-model/src/main/java/com/food/ordering/system/kafka/order/avro/model/CustomerAvroModel.java
+++ infrastructure/kafka/kafka-model/src/main/java/com/food/ordering/system/kafka/order/avro/model/CustomerAvroModel.java
@@ -5,7 +5,6 @@
  */
 package com.food.ordering.system.kafka.order.avro.model;
 
-import org.apache.avro.generic.GenericArray;
 import org.apache.avro.specific.SpecificData;
 import org.apache.avro.util.Utf8;
 import org.apache.avro.message.BinaryMessageEncoder;
